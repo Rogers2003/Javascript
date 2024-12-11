ecf328d6cc841a82989ee468b69016b488712975
Rogers2003 <steverogers123x@gmail.com>
2024-12-11 04:37:07 +0000

    introduction to variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 10101010
+let accountEmail = "virdhoriya@gmail.com"
+accountPassword = "123456788"
+accountCity = "konoha"
+
+
+// accountId = 1000
+accountEmail = " steverogers123@gmail.com"
+accountPassword = "virji@123"
+accountCity = "hidden rain village"
+// console.log(accountEmail);
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
